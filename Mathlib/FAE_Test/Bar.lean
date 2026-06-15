d9a740564b9bd4a90b311d80df3b43728b7c99eb
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2026-06-15 05:30:51 +0200

    created example files

---
+++ Mathlib/FAE_Test/Bar.lean
@@ -0,0 +1,23 @@
+/-
+Copyright (c) 2026 Filippo A. E. Nuccio. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Filippo A. E. Nuccio
+-/
+
+module
+
+import Mathlib.Data.Nat.Basic
+public import Mathlib.FAE_Test.Foo
+
+public section
+
+lemma irred_nonexposed_bar : irred_nonexposed_foo = 23 := by
+  unfold irred_nonexposed_foo ; rfl
+
+lemma irred_nonexposed_bar' : irred_nonexposed_foo = 23 := by
+  apply irred_nonexposed_foo_def
+
+lemma irred_exposed_bar : irred_exposed_foo = 23 := by
+  unfold irred_exposed_foo ; rfl
+
+lemma bar : semired_exposed_foo = 23 := rfl
---
+++ Mathlib/FAE_Test/Foo.lean
@@ -0,0 +1,28 @@
+/-
+Copyright (c) 2026 Filippo A. E. Nuccio. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Filippo A. E. Nuccio
+-/
+
+module
+
+import Mathlib.Data.Nat.Basic
+
+public section
+
+@[irreducible]
+def irred_nonexposed_foo : ℕ := 23
+
+lemma irred_nonexposed_foo_def : irred_nonexposed_foo = 23 := by
+  unfold irred_nonexposed_foo ; rfl
+
+@[expose, irreducible]
+def irred_exposed_foo : ℕ := 23
+
+lemma irred_exposed_foo_def : irred_exposed_foo = 23 := by
+  unfold irred_exposed_foo ; rfl
+
+@[expose]
+def semired_exposed_foo : ℕ := 23
+
+lemma semired_exposed_foo_def : semired_exposed_foo = 23 := rfl
